00661f41f38b0e1ee557ff07974e86fd0e58a253
lpett <lpett@uvm.edu>
2018-01-31 14:29:14 -0500

    Homework3

---
+++ FirstProject/BasicCoding.R
@@ -0,0 +1,12 @@
+# Basic R commands and usage
+# 30 January 2018
+# LAP
+
+#Using the assignment operator
+
+x <- 5 # preferred 
+print(x)
+y = 4 # legal but not used except in function
+y = y+1.1
+y <- y+1
+plantHeight <- 5.5
